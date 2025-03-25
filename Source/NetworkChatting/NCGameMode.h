02fbc48f00a48f0492aebaecdbe77ed628e756ca
KimLink <Linkedcirrus93@gmail.com>
2025-03-25 15:20:09 +0900

    StartChattingServer

---
+++ Source/NetworkChatting/NCGameMode.h
@@ -0,0 +1,17 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameMode.h"
+#include "NCGameMode.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class NETWORKCHATTING_API ANCGameMode : public AGameMode
+{
+	GENERATED_BODY()
+	
+};
---
+++ Source/NetworkChatting/NCPlayerController.h
@@ -0,0 +1,17 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/PlayerController.h"
+#include "NCPlayerController.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class NETWORKCHATTING_API ANCPlayerController : public APlayerController
+{
+	GENERATED_BODY()
+	
+};
